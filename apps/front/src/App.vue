751fabf0e70a44ffa27590248076f2e60e4e471b
Gustavo Barragan <dev@droni.co>
2026-09-23 13:26:15 -0500

    feat: chat init ui

--- apps/front/src/App.vue
+++ apps/front/src/App.vue
@@ -1,11 +1,20 @@
 
 <template>
-  <div class="dark">
-    <DuiButton>Hola</DuiButton>
-    <h1>Hola mundo</h1>
+  <div class="container mx-auto h-screen border flex flex-col justify-around transition">
+    <section v-if="activeChat" class="transition border bg-rose-300 h-full">
+      <ChatPanel />
+    </section>
+    <section class="p-4">
+      <DuiTextarea />
+      <DuiButton>
+        <i class="mdi mdi-access-point-check" /> Enviar
+      </DuiButton>
+    </section>
   </div>
 </template>
 <script setup lang="ts">
-import { DuiButton } from '@dronico/droni-kit';
-
+import { ref}  from 'vue'
+import ChatPanel from './components/ChatPanel.vue';
+import { DuiButton, DuiTextarea } from '@dronico/droni-kit';
+const activeChat = ref(false)
 </script>
---
+++ apps/front/src/components/ChatPanel.vue
@@ -0,0 +1,7 @@
+<template>
+  <DuiCard title="Chat con agente" />
+</template>
+<script setup lang="ts">
+import { DuiCard } from '@dronico/droni-kit';
+
+</script>
