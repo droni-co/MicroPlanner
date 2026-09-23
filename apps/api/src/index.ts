19b7433d947b6a2c53be42010931914bcc053fd8
Gustavo Barragan <dev@droni.co>
2026-09-23 11:51:48 -0500

    chore: turbo

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,16 @@
-console.log('hola')
+import express from 'express'
+import morgan from 'morgan'
+
+
+const app = express()
+app.use(morgan('tiny'))
+app.disable('x-powered-by')
+const port = 3000
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
